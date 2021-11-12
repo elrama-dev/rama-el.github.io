351795cb3cbd17ca230e2cd38025454755550bd7
rama-el <75620554+rama-el@users.noreply.github.com>
2021-11-12 14:25:21 +0700

    script buat menu dan link menu

---
+++ script.js
@@ -0,0 +1,19 @@
+function openMenu(){
+  var menuIcon= document.getElementById("menu-icon");
+  var menuContent = document.getElementsByClassName("menu-content")[0];
+  if(menuContent.classList.length == 1){
+    menuIcon.classList.remove("fa-bars");
+    menuIcon.classList.add("fa-times");
+    menuContent.classList.add("opened");
+  } else {
+    menuIcon.classList.remove("fa-times");
+    menuIcon.classList.add("fa-bars");
+    menuContent.classList.remove("opened");
+  }
+}
+function toSection(id){
+  var section = document.getElementById(id);
+  openMenu();
+  section.style.color = "red";
+  setTimeout(function(){section.style.color = "black";}, 1000);
+}
